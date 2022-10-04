360e72cec30d928bfa6cddad152a7502cef411ad
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-10-04 16:41:52 +0900

    課題9完了

--- 09_chapter/answer.rb
+++ 09_chapter/answer.rb
@@ -1,11 +1,30 @@
 module NiceVoiceFunction
+  def beautiful_voice
+    "#{call}(自動で音声を美しくします)"
+  end
 end
 
 module Runteq
+  def commercial_message
+    'https://youtu.be/LCrCsF4Ro3M'
+  end
 end
 
 module Runtekun
+  NAME = 'らんてくん'
+  BIRTHDAY =  '7月8日'
 end
 
 class MobilePhone
+  include NiceVoiceFunction
+  extend Runteq
+  include Runtekun
+
+  def call
+    '通話機能'
+  end
+
+  def digital_runtekun
+    "初めまして！僕、#{NAME}です！誕生日は#{BIRTHDAY}だぞ！"
+  end
 end
