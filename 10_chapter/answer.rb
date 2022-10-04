81f0a5ebe2fbe96d40858907ac26bad120de35d8
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-10-04 17:08:14 +0900

    課題10完了

--- 10_chapter/answer.rb
+++ 10_chapter/answer.rb
@@ -3,10 +3,18 @@ require 'uri'
 require 'json'
 
 def chapter10_01
+  uri = URI.parse('https://example.com/')
+  Net::HTTP.get uri
 end
 
 def chapter10_02
+  json = "{\"runteq\":{\"name\":\"らんてくん\"}}"
+  JSON.parse json
 end
 
 def chapter10_03
+  zipcode = '150-0042'
+  uri = URI.parse("https://zipcloud.ibsnet.co.jp/api/search?zipcode=#{zipcode}")
+  res = Net::HTTP.get uri
+  JSON.parse res
 end
